3bee8169683137e54da031d94b56aaa72767d2a4
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-06 19:22:24 +0200

    features/add message in components-02

--- react-exercises/src/index.jsx
+++ react-exercises/src/index.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import {createRoot} from "react-dom/client"
 import { App } from "./Components-03";
-import { Hello } from "./Components-02";
+
 
 
 const root = createRoot(document.getElementById("root"))
 
 const appEl = <App/>
-const HelloEl = <Hello/>
 
-root.render(
-    <>
-    {HelloEl}
-    {appEl}
-    </>
-    
-)
+root.render(appEl)
 
