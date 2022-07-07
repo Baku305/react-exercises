fdab7625334773237a7aed821b18057173c7fa40
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-07 14:48:43 +0200

    features/props-04 (strong tag prop name)

--- react-exercises/src/Components-03.jsx
+++ react-exercises/src/Components-03.jsx
@@ -3,11 +3,13 @@ import { Hello } from "./Components-02";
 import { Welcome } from "./Props-01";
 import { Welcome2 } from "./Props-02";
 import { Welcome3 } from "./Props-03";
+import { Welcome4 } from "./props-04";
 
 const HelloEl = <Hello />;
-const WelcomeEl = <Welcome name="Francesco" />;
+const WelcomeEl = <Welcome name = "Francesco" />;
 const Welcome2El = <Welcome2 />;
-const Welcome3El = <Welcome3 name="Giovanni" age="28" />;
+const Welcome3El = <Welcome3 name ="Giovanni" age="28" />;
+const Welcome4EL = <Welcome4 name = "Marco"/>
 
 export class App extends React.Component {
   render() {
@@ -17,6 +19,7 @@ export class App extends React.Component {
         <span>{WelcomeEl}</span>
         <span>{Welcome2El}</span>
         <span>{Welcome3El}</span>
+        <span>{Welcome4EL}</span>
       </div>
     );
   }
---
+++ react-exercises/src/props-04.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export class Welcome4 extends React.Component{
+    render(){
+        return <p>Welcome <strong>{this.props.name}</strong> </p>
+    }
+}
