e73f2aff0be8717075b8e59dd076f9266d1c2879
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-07 14:19:47 +0200

    features/props-01 (Welcome class)

--- react-exercises/src/Components-03.jsx
+++ react-exercises/src/Components-03.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hello } from "./Components-02";
+import { Welcome } from "./Props-01";
 
 const HelloEl = <Hello/>
+const WelcomeEl = <Welcome name = "Francesco"/>
 
 export class App extends React.Component{
     render(){
-        return <div>{HelloEl}</div>
+        return <div>{HelloEl} {WelcomeEl}</div>
     }
 }
