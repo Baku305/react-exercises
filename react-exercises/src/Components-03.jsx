f564d3d4842b0c961e8ae7c29c03f4478deb5c30
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-07 14:32:29 +0200

    features/props-02 (set default props)

--- react-exercises/src/Components-03.jsx
+++ react-exercises/src/Components-03.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Hello } from "./Components-02";
 import { Welcome } from "./Props-01";
+import { Welcome2 } from "./Props-02";
 
 const HelloEl = <Hello/>
 const WelcomeEl = <Welcome name = "Francesco"/>
+const Welcome2El = <Welcome2 />
 
 export class App extends React.Component{
     render(){
-        return <div>{HelloEl} {WelcomeEl}</div>
+        return <div>{HelloEl} {WelcomeEl} {Welcome2El}</div>
     }
 }
