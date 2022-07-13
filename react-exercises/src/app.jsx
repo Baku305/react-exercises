791ba8a8e73ad4a836eecb621882704991b306c1
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-13 15:23:35 +0200

    features/ClickTracker

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
+import ClickTracker from "./ClickTracker ";
 
 export class App extends React.Component {
   render() {
     return (
+        <>
         <div>
-          <ClickCounter/>
+        <ClickCounter />
+      </div>
+      <div>
+          <ClickTracker />
         </div>
+        </>
     );
   }
 }
