c8f314ba9a9ed80e0ad656caef1bfb6522a4e688
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-13 08:51:15 +0200

    last fix

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { Hello } from "./Components-02";
-import { Welcome } from "./Props-01";
-import { Welcome2 } from "./Props-02";
-import { Welcome3 } from "./Props-03";
-import { Welcome4 } from "./props-04";
 import { Welcome5 } from "./props-05";
 
 export class App extends React.Component {
