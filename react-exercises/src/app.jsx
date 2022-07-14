db1f89112c56eaf4007b77b5c35ecce21ea91af3
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-14 13:56:35 +0200

    fix : app name

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import ClickTracker from "./ClickTracker ";
-import InteractiveWelcome from "./InteractiveWelcome ";
 import Login from "./Login";
 
 export class App extends React.Component {
 	render() {
 		return (
 			<>
-				<div>
-					<ClickCounter />
-				</div>
-				<div>
-					<ClickTracker />
-				</div>
-				<div>
-					<InteractiveWelcome />
-				</div>
 				<div>
 					<Login />
 				</div>
