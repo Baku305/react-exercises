220fa3557c4333b75abcc6abf83f9ebeda934605
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-14 16:50:42 +0200

    fix : clean files

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import ClickTracker from "./ClickTracker ";
-import InteractiveWelcome from "./InteractiveWelcome ";
-import Login from "./Login";
 import TodoList from "./TodoList";
-import UncontrolledLogin from "./UncontrolledLogin";
+
 
 export class App extends React.Component {
 
@@ -13,21 +9,6 @@ export class App extends React.Component {
   render() {
     return (
         <>
-        <div>
-        <ClickCounter />
-      </div>
-      <div>
-          <ClickTracker />
-        </div>
-        <div>
-          <InteractiveWelcome/>
-        </div>
-        <div>
-          <Login onLogin = {this.onLogin}/>
-        </div>
-        <div>
-          <UncontrolledLogin/>
-        </div>
         <div>
           <TodoList/>
         </div>
