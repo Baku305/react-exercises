e9945fe61d85b5e3644b2c3959c36f2ea5540204
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-14 14:06:02 +0200

    feat : add reset button

--- react-exercises/src/Login.jsx
+++ react-exercises/src/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default class Login extends React.Component {
-
 	state = {
 		username: "",
 		password: "",
@@ -17,8 +16,18 @@ export default class Login extends React.Component {
 				event.target.type === "checkbox"
 					? event.target.checked
 					: event.target.value,
-			
-			compiled: this.state.username !== "" && this.state.password !== "" ? true : false
+
+			compiled:
+				this.state.username !== "" && this.state.password !== "" ? true : false,
+		});
+	};
+
+	eventReset = () => {
+		this.setState({
+			username: "",
+			password: "",
+			remember: true,
+			compiled: false,
 		});
 	};
 
@@ -28,29 +37,31 @@ export default class Login extends React.Component {
 				<div>
 					<div>
 						<h3>LOGIN</h3>
-					<input
-						type="text"
-						name="username"
-						value={this.state.username}
-						onChange={this.eventHandler}
-					/>
-					<input
-						type="password"
-						name="password"
-						value={this.state.password}
-						onChange={this.eventHandler}
-					/>
-					<input
-						type="checkbox"
-						name="remember"
-						checked={this.state.remember}
-						onChange={this.eventHandler}
-					/>
+						<input
+							type="text"
+							name="username"
+							value={this.state.username}
+							onChange={this.eventHandler}
+						/>
+						<input
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.eventHandler}
+						/>
+						<input
+							type="checkbox"
+							name="remember"
+							checked={this.state.remember}
+							onChange={this.eventHandler}
+						/>
 					</div>
-					<button onClick={this.onLogin} disabled = {!this.state.compiled}>LOGIN</button>
+					<button onClick={this.onLogin} disabled={!this.state.compiled}>
+						LOGIN
+					</button>
+					<button onClick={this.eventReset}>RESET</button>
 				</div>
 			</>
 		);
 	}
 }
-
