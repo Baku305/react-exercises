c27287ab066cd095ef9f51cc043aac9a9de22c8c
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-22 13:56:59 +0200

    fix/clean folder

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import ClickTracker from "./ClickTracker ";
 import InteractiveWelcome from "./InteractiveWelcome ";
-import Login from "./Login";
-import TodoList from "./TodoList";
-import UncontrolledLogin from "./UncontrolledLogin";
-import { Welcome } from "./Welcome";
+
 
 export class App extends React.Component {
 
@@ -15,25 +10,7 @@ export class App extends React.Component {
     return (
         <>
         <div>
-        <ClickCounter />
-      </div>
-      <div>
-          <ClickTracker />
-        </div>
-        <div>
           <InteractiveWelcome/>
-        </div>
-        <div>
-          <Login onLogin = {this.onLogin}/>
-        </div>
-        <div>
-          <UncontrolledLogin/>
-        </div>
-        <div>
-          <TodoList/>
-        </div>
-        <div>
-          <Welcome/>
         </div>
         </>
     );
