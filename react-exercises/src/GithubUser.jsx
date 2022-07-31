ea3ba312ab9a24a5f7c6afe80916e3a8db886811
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-31 15:24:54 +0200

    fixes

--- react-exercises/src/GithubUser.jsx
+++ react-exercises/src/GithubUser.jsx
@@ -1,41 +1,42 @@
+
 import { useEffect } from "react";
 import { useState } from "react";
 
+
 export function GithubUser({ username }) {
+
+
   const [data, setData] = useState({});
-  const [error, setError] = useState({});
+  const [error,setError] = useState({})
+  const [status,setStatus] = useState("")
 
   const getUser = async (username) => {
-    try {
-      const fetchUser = `https://api.github.com/users/${username}`;
 
-      const fetching = await fetch(fetchUser);
+   try {
+    const fetchUser = `https://api.github.com/users/${username}`;
+  
+    const fetching = await fetch(fetchUser);
 
-      const json = await fetching.json();
+    setStatus(fetching.status)
+  
+    const json = await fetching.json();
 
-      setData(json);
-
-      console.log(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    }
+    setData(json)
+   } catch (error) {
+    setError(error)
+    setData(null)
+   }
   };
 
-  useEffect(() => {
-    getUser(username);
-  }, [username]);
+  useEffect(()=>{
+
+   getUser(username)
+
+  },[username])
 
   return (
     <>
-      {data !== {} ? (
-        <div>
-          <h1>{data.login}</h1>
-          <p>{data.url}</p>
-        </div>
-      ) : (
-        <h1>{error.message}</h1>
-      )}
+    {status === 200 ? <h1>{data.login}</h1> : <h1>{data.message}</h1>}
     </>
   );
 }
