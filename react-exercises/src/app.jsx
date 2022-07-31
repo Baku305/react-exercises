1e412765add42618480f9754a88895354795b684
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-31 10:13:06 +0200

    clean files

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,27 +1,11 @@
 import React from "react";
 
-import { Sum } from "./Sum";
-import { Welcome } from "./Welcome";
-import Login from "./Login";
-import {ClickCounter} from "./ClickCounter"
 import { Counter } from "./Counter";
-
-const onLogin = (state) => {
-  console.log(state);
-};
-
-const onCounterChange = (state) => {
-  console.log(state);
-}
 
 export function App() {
   return (
     <>
       <div>
-        <Welcome />
-        <Sum />
-        <Login onLogin={onLogin} />
-        <ClickCounter onCounterChange = {onCounterChange}/>
         <Counter/>
       </div>
     </>
