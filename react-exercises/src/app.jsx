f8e694024a30bb2c994601347d4264be0d15bdb7
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-31 08:14:56 +0200

    clean files

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -5,24 +5,10 @@ import {ClickCounter} from "./ClickCounter"
 
 
 export class App extends React.Component {
-  state = {
-    language: "en",
-  };
 
-  handleLenguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-    console.log(this.state);
-  };
-
-  onLogin = (state) => console.log(state);
- 
   render() {
     return (
       <>
-        <Welcome />
-								<Sum/>
 								<ClickCounter/>
       </>
     );
