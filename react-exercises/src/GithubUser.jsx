cf39d051dd1749cac9b7a31dbc0323538b0cbe45
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-01 17:42:02 +0200

    feat/useGithubuser

--- react-exercises/src/GithubUser.jsx
+++ react-exercises/src/GithubUser.jsx
@@ -1,37 +1,40 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
+import { useGitubUser } from "./useGithubUser";
 
 
 export function GithubUser({ username }) {
 
 
-  const [data, setData] = useState({});
-  const [error,setError] = useState({})
+  // const [data, setData] = useState({});
+  // const [error,setError] = useState({})
 
-  const getUser = async (username) => {
+  // const getUser = async (username) => {
 
-   try {
-    const fetchUser = `https://api.github.com/users/${username}`;
+  //  try {
+  //   const fetchUser = `https://api.github.com/users/${username}`;
   
-    const fetching = await fetch(fetchUser);
+  //   const fetching = await fetch(fetchUser);
   
-    const json = await fetching.json();
+  //   const json = await fetching.json();
 
-    setData(json)
+  //   setData(json)
   
-    console.log(json)
-   } catch (error) {
-    setError(error)
-    setData(null)
-   }
-  };
+  //   console.log(json)
+  //  } catch (error) {
+  //   setError(error)
+  //   setData(null)
+  //  }
+  // };
 
-  useEffect(()=>{
+  // useEffect(()=>{
 
-   getUser(username)
+  //  getUser(username)
 
-  },[username])
+  // },[username])
+
+  const {data, error} = useGitubUser({username})
 
   return (
     <>
---
+++ react-exercises/src/useGithubUser.jsx
@@ -0,0 +1,34 @@
+
+import { useState, useEffect } from "react";
+
+export function useGitubUser({username}){
+
+ const [data, setData] = useState({});
+ const [error,setError] = useState({})
+
+ const getUser = async (username) => {
+
+  try {
+   const fetchUser = `https://api.github.com/users/${username}`;
+ 
+   const fetching = await fetch(fetchUser);
+ 
+   const json = await fetching.json();
+
+   setData(json)
+ 
+   console.log(json)
+  } catch (error) {
+   setError(error)
+   setData(null)
+  }
+ };
+
+ useEffect(()=>{
+
+  getUser(username)
+
+ },[username])
+
+ return {data,error}
+}
