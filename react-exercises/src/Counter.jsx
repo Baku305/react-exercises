c93aecfee6131faf5eb125c3c7288cc0f76b9451
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-03 17:38:08 +0200

    feat/useCallBack

--- react-exercises/src/Counter.jsx
+++ react-exercises/src/Counter.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import {UseCounter} from "./useCounter"
 
 
     export function Counter(){
 
         const [count, setCount] = useState(0)
 
+        const memoizeCounter = useCallback(()=>UseCounter(count),[count])
+        memoizeCounter()
+
         useEffect(()=>{
+            
             const interval = setInterval(()=>{
                 setCount(count + 1)
             },1000)
+            
             return () => {
                 clearInterval(interval)
             }
+            
         },[count])
 
 
---
+++ react-exercises/src/useCounter.jsx
@@ -0,0 +1,15 @@
+
+import { useEffect } from "react";
+import { useState } from "react"
+
+
+export function UseCounter(amount){
+ 
+ const [count, setCount] = useState(0)
+
+ useEffect(()=>{
+  setCount(amount)
+ },[amount])
+
+console.log(`count by useCounter:${count}`);
+}
