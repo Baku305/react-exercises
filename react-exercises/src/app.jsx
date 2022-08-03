e8eae28efdec050bcf3c90bc6155788b7053d9d1
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-03 15:58:43 +0200

    feat/useContext-02

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import { Sum } from "./Sum";
 import { Welcome } from "./Welcome";
 import Login from "./Login";
-import {ClickCounter} from "./ClickCounter"
+import { ClickCounter } from "./ClickCounter";
 import { Counter } from "./Counter";
 import { GithubUser } from "./GithubUser";
+import { LanguageContext } from "./LanguageContext";
+import { useState } from "react";
+import { DisplayLanguage } from "./DisplayLanguage";
 
 const onLogin = (state) => {
   console.log(state);
@@ -13,19 +16,30 @@ const onLogin = (state) => {
 
 const onCounterChange = (state) => {
   console.log(state);
-}
+};
 
 export function App() {
+  const [language, setlanguage] = useState("en");
+
+  const handleChangeLanguage = (event) => {
+    setlanguage(event.target.value);
+  };
   return (
     <>
       <div>
-        <Welcome />
-        <Sum />
-        <Login onLogin={onLogin} />
-        <ClickCounter onCounterChange = {onCounterChange}/>
-        <Counter/>
-        <GithubUser username= "Baku305"/>
+        <select name="" id="" onChange={handleChangeLanguage}>
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={language}>
+          <Welcome />
+          <Sum />
+          <Login onLogin={onLogin} />
+          <ClickCounter onCounterChange={onCounterChange} />
+          <Counter />
+          <GithubUser username="Baku305" />
+        </LanguageContext.Provider>
       </div>
     </>
-  );}
-
+  );
+}
