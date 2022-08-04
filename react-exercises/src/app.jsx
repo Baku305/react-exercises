5a03e7175027d32537890086b9940ea6be5b74f9
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-04 14:52:40 +0200

    clean files

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 
-import { Sum } from "./Sum";
-import { Welcome } from "./Welcome";
-import Login from "./Login";
-import {ClickCounter} from "./ClickCounter"
-import { Counter } from "./Counter";
-import { GithubUser } from "./GithubUser";
+
 import { CarDetails } from "./CarDetails";
 
-const onLogin = (state) => {
-  console.log(state);
-};
-
-const onCounterChange = (state) => {
-  console.log(state);
-}
 
 export function App() {
   return (
     <>
       <div>
-        <Welcome />
-        <Sum />
-        <Login onLogin={onLogin} />
-        <ClickCounter onCounterChange = {onCounterChange}/>
-        <Counter/>
-        <GithubUser username= "Baku305"/>
         <CarDetails initialData = {{model : "BMW", year : "2020", color : "blue"}}/>
       </div>
     </>
