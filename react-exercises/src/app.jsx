b96e3bf85a45328637c58f3e614946e1d73ebf39
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-04 15:01:17 +0200

    feat/cleanCounter

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 
-import { Sum } from "./Sum";
-import { Welcome } from "./Welcome";
-import Login from "./Login";
-import {ClickCounter} from "./ClickCounter"
 import { Counter } from "./Counter";
-import { GithubUser } from "./GithubUser";
 
-const onLogin = (state) => {
-  console.log(state);
-};
-
-const onCounterChange = (state) => {
-  console.log(state);
-}
 
 export function App() {
   return (
     <>
       <div>
-        <Welcome />
-        <Sum />
-        <Login onLogin={onLogin} />
-        <ClickCounter onCounterChange = {onCounterChange}/>
         <Counter/>
-        <GithubUser username= "Baku305"/>
       </div>
     </>
   );}
