05d3f376dd63bc1e474d4b73af24af6e96406536
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-04 14:19:38 +0200

    feat/ custom-hooks-02

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 
-import { Sum } from "./Sum";
-import { Welcome } from "./Welcome";
 import Login from "./Login";
-import {ClickCounter} from "./ClickCounter"
-import { Counter } from "./Counter";
-import { GithubUser } from "./GithubUser";
 
 const onLogin = (state) => {
   console.log(state);
 };
 
-const onCounterChange = (state) => {
-  console.log(state);
-}
-
 export function App() {
   return (
     <>
       <div>
-        <Welcome />
-        <Sum />
         <Login onLogin={onLogin} />
-        <ClickCounter onCounterChange = {onCounterChange}/>
-        <Counter/>
-        <GithubUser username= "Baku305"/>
       </div>
     </>
-  );}
-
+  );
+}
