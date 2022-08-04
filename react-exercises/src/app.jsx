a9eb83a6cdbcf22d7541ee3a78447ae704d71abb
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-04 19:06:20 +0200

    feat/react-router-02

--- react-exercises/src/app.jsx
+++ react-exercises/src/app.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Welcome } from "./Welcome";
-import {Routes, Route} from 'react-router-dom'
-
+import { Routes, Route } from "react-router-dom";
+import { ClickCounter } from "./Counter";
 
 export function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path ="/" element = {<Welcome name ="ERIK"/>}></Route>
+          <Route path="/" element={<Welcome name="ERIK" />}></Route>
+          <Route path="/counter" element={<ClickCounter />}></Route>
         </Routes>
       </div>
     </>
