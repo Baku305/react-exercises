da49f74468a57610c8e3801b27899fa741f1c755
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-22 13:05:52 +0200

    some fixes

--- react-exercises/src/Login.jsx
+++ react-exercises/src/Login.jsx
@@ -1,29 +1,23 @@
 import { useEffect, useState } from "react";
 import { useLogin } from "./useLogin";
 
-
-export default function Login({onLogin}) {
-
-    
-
+export default function Login({ onLogin }) {
   const [userInfo, setUserInfo] = useState({
     username: "",
     password: "",
     remember: false,
-				compiled: false
-  })
-  
-  const {username,password} = useLogin(userInfo.username,userInfo.password)
-  console.log(`you username is: ${username} and you password is ${password}`)
+    compiled: false,
+  });
+
+  const { username, password } = useLogin(userInfo.username, userInfo.password);
+  console.log(`you username is: ${username} and you password is ${password}`);
 
   const eventHandler = (event) => {
-			setUserInfo(
-					{
-						...userInfo,
-						      [event.target.name]:
+    setUserInfo({
+      ...userInfo,
+      [event.target.name]:
         event.target.type === "checkbox" ? event.target.checked : event.target.value,
-					}
-    );
+    });
   };
 
   return (
@@ -31,12 +25,7 @@ export default function Login({onLogin}) {
       <div>
         <div>
           <h3>LOGIN</h3>
-          <input
-            type="text"
-            name="username"
-            value={userInfo.username}
-            onChange={eventHandler}
-          />
+          <input type="text" name="username" value={userInfo.username} onChange={eventHandler} />
           <input
             type="password"
             name="password"
@@ -50,7 +39,10 @@ export default function Login({onLogin}) {
             onChange={eventHandler}
           />
         </div>
-        <button onClick={() => onLogin(userInfo)} disabled={userInfo.username === "" || userInfo.password === ""}>
+        <button
+          onClick={() => onLogin(userInfo)}
+          disabled={userInfo.username === "" || userInfo.password === ""}
+        >
           LOGIN
         </button>
       </div>
